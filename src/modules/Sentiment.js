d353879b4cea7c29c1af63684bc20e392928592b
siqister <siqi.zhu@gmail.com>
2017-04-10 23:15:18 -0400

    style changes

--- src/modules/Sentiment.js
+++ src/modules/Sentiment.js
@@ -6,10 +6,10 @@ function Sentiment(dom){
 
 	const _svg = d3.select(dom).select('svg').size()===0?d3.select(dom).append('svg'):d3.select(dom).select('svg');
 	const _graphic = _svg.select('.graphic').size()===0?_svg.append('g').attr('class','graphic'):_svg.select('.graphic');
-	const _m = {t:20, r:50, l:70, b:50};
+	const _m = {t:30, r:50, l:70, b:30};
 	const _w = Math.floor((dom.clientWidth - _m.l - _m.r)/config.textNodeMinSizeX)*config.textNodeMinSizeX-40,
 		_h = dom.clientHeight - _m.t - _m.b,
-		_r = 5;
+		_r = 6;
 
 	const _scaleX = d3.scaleLinear().domain([-1,1]).range([0,_w]), //score
 		_scaleY = d3.scaleLinear().domain([0,5]).range([_h,0]); //magnitude
@@ -28,6 +28,10 @@ function Sentiment(dom){
 			.data([1])
 			.enter()
 			.append('g').attr('class','background');
+		background.append('rect')
+			.style('fill','white')
+			.attr('width',_w)
+			.attr('height',_h);
 		background.append('line')
 			.attr('x1',_w/2)
 			.attr('x2',_w/2)
@@ -35,13 +39,13 @@ function Sentiment(dom){
 			.attr('y2',_h)
 			.style('stroke','#ccc')
 			.style('stroke-width','1px');
-		background.append('line')
+/*		background.append('line')
 			.attr('x1',0)
 			.attr('x2',_w)
 			.attr('y1',_h)
 			.attr('y2',_h)
 			.style('stroke','#ccc')
-			.style('stroke-width','1px');
+			.style('stroke-width','1px');*/
 
 		let node = _graphic.selectAll('.comment')
 			.data(docs.filter(doc=>doc),d=>d.id);
@@ -49,13 +53,19 @@ function Sentiment(dom){
 			.append('g').attr('class','comment')
 			.attr('transform',d=>`translate(${_scaleX(d.sentiment.score)},${_scaleY(d.sentiment.magnitude)})`);
 		nodeEnter.append('circle')
+			.attr('class','outer')
 			.attr('r',0)
-			.style('fill-opacity',.2)
-			.style('stroke','rgb(100,100,100)')
+			.style('fill-opacity',.1)
+			.style('fill','#03afeb')
+			.style('stroke','#03afeb')
+			.style('stroke-opacity',.5)
 			.style('stroke-width','2px');
-
+		nodeEnter.append('circle')
+			.attr('r',2)
+			.style('fill-opacity',.5)
+			.style('fill','#03afeb');
 		nodeEnter.merge(node)
-			.selectAll('circle')
+			.selectAll('.outer')
 			.transition().duration(1000)
 			.attr('r',_r)
 	}
